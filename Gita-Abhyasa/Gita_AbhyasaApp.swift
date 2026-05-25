4147dcc36ccc4b77faa4147d625afd24db89e721
Ramesh Parthasarathy <gitapriyam@gmail.com>
2026-05-25 12:13:34 -0700

    Initial Commit

---
+++ Gita-Abhyasa/Gita_AbhyasaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gita_AbhyasaApp.swift
+//  Gita-Abhyasa
+//
+//  Created by Ramesh Parthasarathy on 5/25/26.
+//
+
+import SwiftUI
+
+@main
+struct Gita_AbhyasaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Gita-AbhyasaTests/Gita_AbhyasaTests.swift
@@ -0,0 +1,19 @@
+//
+//  Gita_AbhyasaTests.swift
+//  Gita-AbhyasaTests
+//
+//  Created by Ramesh Parthasarathy on 5/25/26.
+//
+
+import Testing
+@testable import Gita_Abhyasa
+
+struct Gita_AbhyasaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
